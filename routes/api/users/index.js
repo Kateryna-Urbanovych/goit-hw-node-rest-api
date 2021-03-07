fe0324a64e0d2d2b2d8c898fbadae4176d4df3ef
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-03-07 17:13:24 +0200

    hw-04: add security (helmet, cloudfront)

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,8 +3,14 @@ const router = express.Router()
 const validate = require('./validation')
 const usersController = require('../../../controllers/users')
 const guard = require('../../../helpers/guard')
+const { createAccountLimiter } = require('../../../helpers/rate-limit-register')
 
-router.post('/auth/register', validate.register, usersController.register)
+router.post(
+    '/auth/register',
+    validate.register,
+    createAccountLimiter,
+    usersController.register,
+)
 router.post('/auth/login', validate.login, usersController.login)
 router.post('/auth/logout', guard, usersController.logout)
 router.post('/current', guard, usersController.current)
